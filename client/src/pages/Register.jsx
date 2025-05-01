1bfce77b1782842876dc7bcc49ed741f03b3196d
llasic00 <llasic00@fesb.hr>
2025-05-01 20:33:10 +0200

    .

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import axios from 'axios';
-import {toast} from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom'
 
 export default function Register() {
@@ -9,25 +8,25 @@ export default function Register() {
         name: '',
         email: '',
         password: '',
+        confirmPassword: '',
     })
 
     const registerUser = async (event) => {
         event.preventDefault()
-        const {name, email, password} = data
+        const {name, email, password, confirmPassword} = data;
+
+        if (data.password !== data.confirmPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+
         try {
-            const {data} = await axios.post('/register', {name, email, password})
-        
-            /*if(data.error){
-                toast.error(data.error)
-            }
-            else{
-                setData({})
-                toast.success('login successful')
-                navigate('/')
-            }*/
+            const {data} = await axios.post('/register', {name, email, password});
+            console.log("Registration success:", data.message);
         }
         catch (error) {
-            console.log(error)
+            console.error(error);
+            alert("Registration failed");
         }
     }
 
@@ -40,6 +39,8 @@ export default function Register() {
             <input type='email' placeholder='E-mail' value={data.email} onChange={(event) => setData({...data, email: event.target.value})} />
             <label>Password</label>
             <input type='password' placeholder='Password' value={data.password} onChange={(event) => setData({...data, password: event.target.value})} />
+            <label>Confirm Password</label>
+            <input type='password' placeholder='Confirm Password' value={data.confirmPassword} onChange={(event) => setData({...data, confirmPassword: event.target.value})} />
 
             <button type='submit'>Submit</button>
         </form>
